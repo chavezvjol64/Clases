3f07e13ac66602d4c886505025d7db1922802292
chavezvjol64 <51514441+chavezvjol64@users.noreply.github.com>
2019-06-10 13:50:47 -0700

    Add files via upload

---
+++ Clase/Clase/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    public class Clsempleados
+    {   //constructor
+        public  Clsempleados()
+        {
+            Nombre = "Alejandro";
+            SueldoDiario = 25.6m;
+            Edad = 25;
+        }
+
+        public string Nombre;
+
+        public decimal SueldoDiario;
+
+        public int Edad;
+
+        //metodo
+
+        public decimal CalcularSalario(int numerodias)
+        {
+            return SueldoDiario * numerodias;
+        }
+    }
+}
---
+++ Clase/Clase/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase
+{
+    class Program
+    {   
+        static void Main(string[] args)
+        {
+            Clsempleados empleado;
+            empleado = new Clsempleados();
+
+            empleado.Edad = 25;
+            empleado.Nombre = "Alejandro";
+            empleado.SueldoDiario = 25.6m;
+            decimal total;
+            total = empleado.CalcularSalario(30);
+
+            Console.WriteLine("El salario mensual del empleado " + empleado.Nombre);
+            Console.WriteLine("Es: " + total.ToString("c"));
+            Console.ReadKey();
+        }
+    }
+}
